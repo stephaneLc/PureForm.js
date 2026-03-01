7df769c6e6216d0a115415a026963cbd35b55352
stephanel <stephane.leclerc2695@gmail.com>
2026-03-01 12:05:50 -0500

    afficher tous le json avec boucle

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 async function getJsonData(){
     try {
         const jsonData = await fetch("../json/formulaire.json");
-        console.log(jsonData);
+        //console.log(jsonData);
         
         if(!jsonData.ok){
              //console.log('Erreur ' + jsonData.status);
@@ -10,14 +10,60 @@ async function getJsonData(){
 
         const infosJson = await jsonData.json(); //créer l'objet Javascript
 
-        console.log(infosJson); //résultat de l'oject 
-        console.log(infosJson.form[0].titre); //afficher le titre du formulaire. 
-        console.log(infosJson.form[0].champs[0]) //afficher l'objet de l'input
+        infosJson.form.forEach((element,index) => {
+            
+            const titreBalise = document.createElement("h2"); //création de la balise
+            titreBalise.innerHTML = infosJson.form[index].titre; //ajouter le titre entre les balises
 
-        const titreBalise = document.createElement("h2"); //création de la balise
-        titreBalise.innerHTML = infosJson.form[0].titre; //ajouter le titre entre les balises
+            document.getElementById("form").appendChild(titreBalise);
 
-        document.getElementById("form").appendChild(titreBalise); //afficher le titre en h2
+            //console.log(element);
+            //console.log(index);
+            //console.log(element.titre); 
+            //console.log(element.champs);
+            //console.log(element.champs[index]);
+            //console.log(element.champs[0].nomBalise);
+
+            //console.log(element.champs[0]);
+
+            element.champs.forEach((champs, indexChamps) => {
+                console.log(champs)
+                console.log(indexChamps);
+
+                const containerInput = document.createElement("div");
+                containerInput.className = "inputLabel";
+
+                const label = document.createElement("label");
+                label.innerHTML = champs.label;
+
+                const errorMessage = document.createElement("p");
+                errorMessage.className = "error";
+                errorMessage.innerHTML = champs.erreurMessage;
+
+                const input = document.createElement(champs.nomBalise); 
+                input.setAttribute("type", champs.type);
+                input.setAttribute("name", champs.name);
+                input.setAttribute("value", "");
+
+                containerInput.appendChild(label);
+                containerInput.appendChild(errorMessage);
+                containerInput.appendChild(input);
+                
+                
+                document.getElementById("form").appendChild(containerInput);
+            });
+
+
+        });
+
+        //console.log(infosJson); //résultat de l'oject 
+        //console.log(infosJson.form[0].titre); //afficher le titre du formulaire. 
+        //console.log(infosJson.form[0].champs[0]) //afficher l'objet de l'input
+
+        //const titreBalise = document.createElement("h2"); //création de la balise
+        //titreBalise.innerHTML = infosJson.form[0].titre; //ajouter le titre entre les balises
+
+        //document.getElementById("form").appendChild(titreBalise); //afficher le titre en h2
 
         const containerInput = document.createElement("div");
         containerInput.className = "inputLabel";
@@ -38,10 +84,10 @@ async function getJsonData(){
         containerInput.appendChild(errorMessage);
         containerInput.appendChild(input);
         
-                            
+        
         document.getElementById("form").appendChild(containerInput);
 
-        
+
        
     } catch (error) {
         console.log("une erreur" + error);
