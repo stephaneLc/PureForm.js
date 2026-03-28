021ab2b867b5ce191f1881e835af35612313dcca
stephanel <stephane.leclerc2695@gmail.com>
2026-03-28 09:02:06 -0400

    créer fonction getJson et affichage du résultat sur la page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const divForm = document.getElementById("divForm");
+
+    getJson(divForm);
+});
+
+async function getJson(divForm){
+    try {
+        const jsonData = await fetch("../json/formulaire.json");
+        
+        if(!jsonData.ok){
+            throw new Error(jsonData.status);
+        }
+
+        const dataJson = await jsonData.json();
+
+        return dataJson;
+
+       
+    } catch (error) {
+        divForm.innerText = "Une erreur est survenu" + ' ' + error;
+    }
+}
+
